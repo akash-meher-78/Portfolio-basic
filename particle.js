ec11c959e47392a9de4c3854aaf2f65909971776
akash-meher-78 <akashmeher6635@gmail.com>
2025-05-13 00:29:15 +0530

    Initial commit

---
+++ particle.js
@@ -0,0 +1,45 @@
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#16ff00" 
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 2,
+      "random": true
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#16ff00",
+      "opacity": 0.3,
+      "width": 1.2
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      }
+    }
+  },
+  "retina_detect": true
+});
